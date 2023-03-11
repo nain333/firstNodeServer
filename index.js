9aff6b9ad1b86d196ead7689cf14053cbbd78cea
@nain333 <hnain70@gmail.com>
2023-03-11 23:57:46 +0530

    read files through the server.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 const port=3000;
 function requestHandeler(request,response){
     console.log(request.url+"The response is: "+ response);
-    response.end("<h1>Gotcha!</h1");
+    response.writeHead(200,{'content-type':'text/html'},{'connection':'zombie'});
+    fs.readFile('./index.htm',function(err,data){
+        if(err){
+            console.log(err);
+            return response.end('<h1>Error!</h1>');
+
+        }
+        return response.end(data);
+    })    
+
 
 }
 var server= http.createServer(requestHandeler);
